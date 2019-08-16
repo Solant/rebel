d3a6c54a30c758260bcff691ed35200230552606
Solant <runner62v6@gmail.com>
2019-08-16 19:02:10 +0300

    improved code generation for typescript

--- compiler/generators/ts/index.ts
+++ compiler/generators/ts/index.ts
@@ -2,12 +2,26 @@ import {BaseType, CustomType, CustomTypeField, TypeTag} from '../../types';
 import {readFileSync} from "fs";
 import { resolve } from 'path';
 
-const nativeTypeMap: { [key: string]: string } = {
-    'i32le': 'number',
-};
+interface NativeTypeInfo {
+    name: string,
+    defaultValue: any,
+}
 
-function generateField(field: CustomTypeField) {
-    return `    ${field.name}: ${nativeTypeMap[field.type.name]}`;
+class CodeGenError extends Error {
+    constructor(m: string) {
+        super(m);
+    }
+}
+
+const nativeTypeMap: Map<string, NativeTypeInfo> = new Map();
+nativeTypeMap.set('i32', { name: 'number', defaultValue: 0 });
+
+function getTypeInfo(field: CustomTypeField): NativeTypeInfo {
+    const res = nativeTypeMap.get(field.type.name);
+    if (!res) {
+        throw new CodeGenError(`Unsupported type ${field.type.name}`);
+    }
+    return res;
 }
 
 function generateInterface(type: CustomType): string {
@@ -18,43 +32,73 @@ function generateInterface(type: CustomType): string {
     return result;
 }
 
+function capitalize() {
+    return function (text: string, render: Function): string {
+        let val = render(text);
+        return val.charAt(0).toUpperCase() + val.slice(1);
+    }
+}
+
+function generateStructRead(type: CustomType): string {
+    const fields = type.props.map(p => ({ name: p.name, value: getTypeInfo(p).defaultValue }));
+    return render(`
+export function read{{#capitalize}}{{name}}{{/capitalize}}(): {{name}} {
+    const var1 = {
+        {{#fields}}
+        {{name}}: {{value}},
+        {{/fields}}
+    };
+    
+    return var1;
+}
+    `, {
+        name: type.name,
+        fields,
+        capitalize,
+    });
+}
+
+function generateMainStructRead(type: CustomType): string {
+    const fields = type.props.map(t => ({ name: t.name, type: t.type.name, nativeType: getTypeInfo(t) }));
+    return render(`
+export function read(buffer: Buffer): {{name}} {
+    const stream = new BimoStream(buffer);
+    
+    const var1 = {
+        {{#fields}}
+        {{name}}: {{nativeType.defaultValue}},
+        {{/fields}}
+    };
+    
+    {{#fields}}
+    var1.{{name}} = stream.read{{#capitalize}}{{type}}{{/capitalize}}();
+    {{/fields}}
+    
+    return var1;
+}
+    `, {
+        name: type.name,
+        fields,
+        capitalize,
+    });
+}
+
 function isCustomType(type: BaseType): type is CustomType {
     return type.tag === TypeTag.Custom;
-}
-
-function injectRuntime() {
-    return readFileSync(resolve(__dirname, 'runtime.js'), { encoding: 'UTF-8' });
-}
-
-function injectMainRead(type: CustomType) {
-    return block(`export function read(buffer): ${type.name}`,
-        block(`const result: ${type.name} =`,
-            type.props.map(generateField)
-        )
-    ).join('\n');
-}
-
-function block(declaration: string, lines: string[]) {
-    let result = [];
-    result.push(`${declaration} {\n`);
-    for (let line of lines) {
-        result.push(line + '\n');
-    }
-    result.push('}');
-
-    return result;
 }
 
 export function generate(types: BaseType[]): string {
     let code = '';
 
-    code += injectRuntime() + '\n';
-
-    code += injectMainRead(types.filter(isCustomType).find(t => t.default)!) + '\n';
-
     code += types
         .filter(isCustomType)
         .map(generateInterface)
+        .join('\n');
+
+    code += types
+        .filter(isCustomType)
+        .filter(t => t.default)
+        .map(generateMainStructRead)
         .join('\n');
 
     return code;
