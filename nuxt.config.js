c874c1fa0317a71d3fa33d24ce5db6b46f287c27
Solant <runner62v6@gmail.com>
2019-08-25 22:01:41 +0300

    Implemented web version

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+export default {
+    srcDir: 'web/',
+    buildModules: [
+        '@nuxt/typescript-build',
+        '@nuxtjs/tailwindcss',
+    ],
+    plugins: [
+        'plugins/composition',
+        { src: 'plugins/codemirror', ssr: false },
+    ],
+    css: [
+        'codemirror/lib/codemirror.css',
+        'codemirror/addon/merge/merge.css',
+        'codemirror/theme/base16-dark.css',
+    ],
+}
