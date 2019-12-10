a1511ba19fcb061411c591bf87d274c481b10d75
Solant <runner62v6@gmail.com>
2019-12-10 15:47:51 +0300

    Implemented generalized ast visitor

--- compiler/visitor.ts
+++ compiler/visitor.ts
@@ -1,6 +1,31 @@
 export type DiscriminateUnion<T, K extends keyof T, V extends T[K]> = T extends Record<K, V> ? T : never;
 
-export interface EnterExitVisitor<T extends B, B, S> {
-    enter?: (node: T, path: B[], scope: S) => any,
-    exit?: (node: T, path: B[], scope: S) => any,
+export interface EnterExitVisitor<T extends B, B, S, R> {
+    enter?: (node: T, path: B[], scope: S) => R,
+    exit?: (node: T, path: B[], scope: S) => R,
+}
+
+interface TaggedInterface<E> {
+    type: E,
+}
+
+// ts-ignore required, as enum can't be described with type system
+// @ts-ignore
+export type GenericVisitor<Enum, NodeType extends TaggedInterface<Enum>, State, Ret> = { [key in Enum]?: EnterExitVisitor<DiscriminateUnion<NodeType, 'type', key>, NodeType, State, Ret> };
+
+function visitHelper<Enum, NodeType extends TaggedInterface<Enum>, State, Ret>(node: NodeType, visitors: GenericVisitor<Enum, NodeType, State, Ret>[], path: NodeType[], scope: State, fun: 'enter' | 'exit'): Ret | undefined {
+    for (let visitor of visitors) {
+        const callback = visitor[node.type] as EnterExitVisitor<NodeType, NodeType, State, Ret> | undefined;
+        if (callback && callback[fun]) {
+            return callback[fun]!(node, path, scope);
+        }
+    }
+}
+
+export function enterNode<Enum, NodeType extends TaggedInterface<Enum>, State, Ret>(node: NodeType, visitors: GenericVisitor<Enum, NodeType, State, Ret>[], path: NodeType[], scope: State): Ret | undefined {
+    return visitHelper(node, visitors, path, scope, 'enter');
+}
+
+export function exitNode<Enum, NodeType extends TaggedInterface<Enum>, State, Ret>(node: NodeType, visitors: GenericVisitor<Enum, NodeType, State, Ret>[], path: NodeType[], scope: State): Ret | undefined {
+    return visitHelper(node, visitors, path, scope, 'exit');
 }
